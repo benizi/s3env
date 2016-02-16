ef3d5444e2631f79fcea27dd0edbfcd4a2ba9890
Benjamin R. Haskell <rust@benizi.com>
2016-02-15 20:11:51 -0500

    Initial commit of s3env command
    Mainly to learn Rust.  Command to parse S3 credentials from various
    tools' normal config files, then spawn a command with those creds
    inserted via env vars.

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+extern crate getopts;
+extern crate ini;
+
+use getopts::Options;
+use std::env;
+use ini::Ini;
+use ini::ini::Properties;
+
+// TODO: I feel like there's a way to combine this with the `use` above...
+type IniErr = ini::ini::Error;
+
+#[derive(Debug)]
+struct S3cfg {
+    access: String,
+    secret: String,
+}
+
+fn ini_err(msg: &str) -> IniErr {
+    IniErr{line: 0, col: 0, msg: msg.to_string()}
+}
+
+fn get_opt(section: &Properties, key: &str) -> Result<String, IniErr> {
+    match section.get(key) {
+        Some(s) => Ok(s.clone()),
+        None => Err(ini_err(&*format!("no {} key", key))),
+    }
+}
+
+fn get_s3cfg(filename: &str, section: &str) -> Result<S3cfg, IniErr> {
+    let ini = try!(Ini::load_from_file(filename));
+    let section = match ini.section(Some(section)) {
+        Some(s) => s,
+        None => return Err(ini_err("no such section")),
+    };
+    let access = try!(get_opt(section, "access_key"));
+    let secret = try!(get_opt(section, "secret_key"));
+    Ok(S3cfg{access: access.to_owned(), secret: secret.to_owned()})
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut options = Options::new();
+    options.optflag("", "s3cmd", "Use s3cmd locations");
+    options.optopt("i", "identity", "Identity to choose", "NAME");
+    options.optopt("", "section", "Identity to choose", "NAME");
+
+    options.optopt("a", "access", "Name of ENV var for Access Key", "NAME");
+    options.optopt("s", "secret", "Name of ENV var for Secret Key", "NAME");
+
+    let matches = match options.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => panic!(e.to_string()),
+    };
+
+    let section = match matches.opts_str(&["i".to_string(),"identity".to_string(),"section".to_string()]) {
+        Some(s) => s,
+        None => "default".to_owned()
+    };
+    println!("s3cmd: {}", matches.opt_present("s3cmd"));
+    let cfg = match get_s3cfg("/home/bhaskell/.s3cfg", &section[..]) {
+        Ok(cfg) => cfg,
+        Err(e) => { println!("Failed: {}", e.msg); std::process::exit(1) }
+    };
+    println!("{:?}", cfg);
+}
